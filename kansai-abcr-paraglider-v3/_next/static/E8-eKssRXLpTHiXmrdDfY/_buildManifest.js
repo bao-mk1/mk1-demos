cf73b3729705df839fd26c4672ee853b24cf5948
bao-mk1 <bao-mk1@users.noreply.github.com>
2026-06-03 09:13:49 +0700

    add: kansai-abcr-paraglider-v3 (fusion of demo 1 and 2 per customer feedback)

---
+++ kansai-abcr-paraglider-v3/_next/static/E8-eKssRXLpTHiXmrdDfY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-56c841ee78cbe577.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
