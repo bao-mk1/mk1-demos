457b0572d691664a7365a194b2383886a9b2ac17
bao-mk1 <84185145+bao-mk1@users.noreply.github.com>
2026-07-17 19:48:52 +0700

    deploy: demo-aidx (2026-07-17 19:48)

---
+++ demo-aidx/_next/static/chunks/app/admin/projects/[id]/page-2091937e88ff98d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[637],{81518:function(e,n,s){Promise.resolve().then(s.bind(s,76831)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[502,138,213,831,971,23,744],function(){return e(e.s=81518)}),_N_E=e.O()}]);
