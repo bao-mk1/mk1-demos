03181adff2894683e848b851e4fdbd7bd22cc278
bao-mk1 <84185145+bao-mk1@users.noreply.github.com>
2026-07-17 12:18:00 +0700

    deploy: demo-fnb (2026-07-17 12:18)

---
+++ demo-fnb/_next/static/chunks/app/layout-b790748134a5c7dc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{37130:function(n,e,i){Promise.resolve().then(i.t.bind(i,58877,23)),Promise.resolve().then(i.bind(i,94768))},58877:function(){}},function(n){n.O(0,[404,768,971,23,744],function(){return n(n.s=37130)}),_N_E=n.O()}]);
