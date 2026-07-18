ed824f7ba17204b157508b5a704aa5af2d3c95a3
bao-mk1 <84185145+bao-mk1@users.noreply.github.com>
2026-07-19 01:38:31 +0700

    deploy: demo-hr (2026-07-19 01:38)

---
+++ demo-hr/_next/static/chunks/app/layout-53f2a9e9b6f1f0f6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{19191:function(e,n,i){Promise.resolve().then(i.t.bind(i,58877,23)),Promise.resolve().then(i.bind(i,94768)),Promise.resolve().then(i.bind(i,13685))},58877:function(){}},function(e){e.O(0,[404,685,768,971,23,744],function(){return e(e.s=19191)}),_N_E=e.O()}]);
